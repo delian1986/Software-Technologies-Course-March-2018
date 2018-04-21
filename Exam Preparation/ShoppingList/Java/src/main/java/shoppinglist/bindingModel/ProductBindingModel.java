493ef234c5505104f457868926aee58b0b888531
delian1986 <delian1914@gmail.com>
2018-04-22 00:00:37 +0300

    shopping list

---
+++ Exam Preparation/ShoppingList/Java/src/main/java/shoppinglist/bindingModel/ProductBindingModel.java
@@ -0,0 +1,58 @@
+package shoppinglist.bindingModel;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+public class ProductBindingModel {
+
+
+    @NotNull
+    private String priority ;
+
+    @NotNull
+
+    private String name;
+
+    @NotNull
+
+    private int quantity ;
+
+    @NotNull
+
+    private String status ;
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
