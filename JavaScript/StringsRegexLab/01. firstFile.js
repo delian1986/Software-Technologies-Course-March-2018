faec5913e1b1f03b11999f0f9219c2311dabf607
delian1986 <delian1914@gmail.com>
2018-04-27 20:21:13 +0300

    Problem 1 file name edited

--- JavaScript/StringsRegexLab/01. firstFile.js
+++
@@ -1,9 +0,0 @@
-function solve(args){
-    let word=args.split('');
-
-    for (let i = 0; i < word.length; i++) {
-        console.log(`str[${i}] -> ${word[i]}`);
-        
-    }
-}
-solve('Hello World');
